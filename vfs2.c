3225a6a74529ab60bd142b6196d7fb9660a66929
sushrut300596 <43106774+sushrut300596@users.noreply.github.com>
2022-09-06 05:42:09 +0000

    changed vfs2.c

--- vfs2.c
+++ vfs2.c
@@ -3,22 +3,27 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <fcntl.h>
+#include <math.h>
 
 // #define SIZE 4294967296
 #define SIZE 1024
 
 typedef struct SB{
-	unsigned char size_disk;
-	unsigned char size_MD;
+	int size_disk;
+	int size_block;
 } SB;
 
 void vdCreate(char *filename) {
+	SB *sb = (SB *)malloc(sizeof(struct SB));
+	sb->size_disk = SIZE;
+	sb->size_block = sb->size_disk/sb->size_disk;
 	int d;
 	char ch = '\0';
 	d = open(filename, O_CREAT | O_WRONLY, 00700);
-	lseek(d, SIZE, SEEK_SET);
+	lseek(d, sb->size_disk, SEEK_SET);
 	write(d, &ch, 1);
-	
+	lseek(d, 0, SEEK_SET);
+
 	close(d);
 }
 
