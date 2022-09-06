0359bdc154b4ed1ceb338e8646f93a3d6b51c6a1
sushrut300596 <43106774+sushrut300596@users.noreply.github.com>
2022-09-06 18:04:53 +0000

    created vfs2vda.c

---
+++ vfs2vda.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <fcntl.h>
+
+void writeFile(char *filename) {
+	int fd;
+	int buf_size = 8;
+	void *buf = (void *)malloc(sizeof(void) * buf_size);
+	fd = open(filename, O_CREAT | O_WRONLY, 00700);
+	int n, count = 0;
+	int i = 0;
+	while(count != SIZE) {
+		while(i < buf_size) {
+			// code
+		}
+		n = write(fd, buf, buf_size);
+		count = count + n;
+	}
+	close(fd);
+}
+
+void readFile(char *filename) {
+	int buf_size = 8;
+	void *buf = (void *)malloc(sizeof(void) * buf_size);
+	int fd;
+	fd = open(filename, O_RDONLY);
+	int i = 0;
+	int n, count = 0;
+	while(count != SIZE) {
+		n = read(fd, buf, buf_size);
+		while(i < buf_size) {
+			// code
+		}
+		count = count + n;
+	}
+	close(fd);	
+}
+
+int main(int argc, char **argv) {
+    
+}
--- vfs2vdc.c
+++ vfs2vdc.c
@@ -13,40 +13,6 @@ typedef struct SB{
 	int size_block;
 } SB;
 
-void writeFile(char *filename) {
-	int fd;
-	int buf_size = 8;
-	void *buf = (void *)malloc(sizeof(void) * buf_size);
-	fd = open(filename, O_CREAT | O_WRONLY, 00700);
-	int n, count = 0;
-	int i = 0;
-	while(count != SIZE) {
-		while(i < buf_size) {
-			// code
-		}
-		n = write(fd, buf, buf_size);
-		count = count + n;
-	}
-	close(fd);
-}
-
-void readFile(char *filename) {
-	int buf_size = 8;
-	void *buf = (void *)malloc(sizeof(void) * buf_size);
-	int fd;
-	fd = open(filename, O_RDONLY);
-	int i = 0;
-	int n, count = 0;
-	while(count != SIZE) {
-		n = read(fd, buf, buf_size);
-		while(i < buf_size) {
-			// code
-		}
-		count = count + n;
-	}
-	close(fd);	
-}
-
 void vdCreate(char *filename) {
 	SB *sb = (SB *)malloc(sizeof(struct SB));
 	sb->size_disk = SIZE;
