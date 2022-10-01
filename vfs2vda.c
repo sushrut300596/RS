50ffc3056ac859a5095c1018397fa8035baf90aa
Sushrut Shendre <sushrut.shendre@gmail.com>
2022-10-01 22:05:42 +0530

    changed vfs2vda.c

--- vfs2vda.c
+++ vfs2vda.c
@@ -15,18 +15,8 @@ typedef struct FMD {
 	int block_no;
 } FMD;
 
-typedef union header {
-	FMD fmd;
-	struct {
-		union header *next;
-	}s;
-} Header;
-
-// Header base;
-// Header *freep = NULL;
-
 void vdAllocate(char *diskname, char *filename) {
-	Header *head = malloc(sizeof(Header));
+	FMD *fmd = malloc(sizeof(FMD));
 	
 	int d, fd;
 	d = open(diskname, O_RDWR);
@@ -34,10 +24,10 @@ void vdAllocate(char *diskname, char *filename) {
 	
 	int size = lseek(fd, 0, SEEK_END);
 	if(size < (2 * FILE_SIZE_MULTIPLE * FILE_SIZE_MULTIPLE)) {
-		head->fmd.file_size = size;
+		fmd->file_size = size;
 	}
-	strcpy(head->fmd.file_name,filename);
-	head->fmd.block_no = 64;
+	strcpy(fmd->file_name,filename);
+	fmd->block_no = 64;
 
 	unsigned char *buf = (unsigned char *)malloc(sizeof(unsigned char) * BUF_SIZE);
 	unsigned char *read_buf = (unsigned char *)malloc(sizeof(unsigned char) * BUF_SIZE);
@@ -46,12 +36,12 @@ void vdAllocate(char *diskname, char *filename) {
 	int n, i, count;
 
 	lseek(d, 2048, SEEK_SET);
-	write(d, head, sizeof(Header));
+	write(d, fmd, sizeof(FMD));
 
 	lseek(fd, 0, SEEK_SET);
 	i = 0;
 	count = 0;
-	while(count < head->fmd.file_size) {
+	while(count < fmd->file_size) {
 		n = read(fd, buf, BUF_SIZE);
 		while(i < BUF_SIZE) {
 			if(i == (BUF_SIZE - 1)) {
@@ -66,7 +56,7 @@ void vdAllocate(char *diskname, char *filename) {
 	lseek(d, 62916, SEEK_SET);
 	i = 0;
 	count = 0;
-	while(count < head->fmd.file_size) {
+	while(count < fmd->file_size) {
 		while(i < BUF_SIZE) {
 			write_buf[i] = read_buf[i];
 			i++;
@@ -75,12 +65,12 @@ void vdAllocate(char *diskname, char *filename) {
 		count = count + n;
 	}
 	
-	lseek(d, 31, SEEK_SET);
+	lseek(d, 32, SEEK_SET);
 	read(d, buf, 224);
 	
-	int no_blocks = my_ceil((double)head->fmd.file_size/BUF_SIZE);
+	int no_blocks = my_ceil((double)fmd->file_size/BUF_SIZE);
 	printf("%d\n", no_blocks);
-	
+
 	i = 0;
 	while(no_blocks >= 0) {
 		if(no_blocks >= 8) {
@@ -92,14 +82,15 @@ void vdAllocate(char *diskname, char *filename) {
 		i++;
 		no_blocks -= 8;
 	}
-	lseek(d, 31, SEEK_SET);
+
+	lseek(d, 32, SEEK_SET);
 	write(d, buf, 224);
 	
 	close(d);
 }
 
 int main(int argc, char **argv) {
-    // printf("%ld\n", sizeof(Header));
+    // printf("%ld\n", sizeof(FMD));
     // exit(1);
 
 	vdAllocate(argv[1], argv[2]);   
