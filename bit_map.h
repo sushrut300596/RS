54768ff1f089df787c0a4da97c4b446708df2eee
Sushrut Shendre <sushrut.shendre@gmail.com>
2022-10-01 15:10:41 +0530

    changed vfs2vda.c

---
+++ bit_map.h
@@ -0,0 +1,4 @@
+#include <stdio.h>
+
+
+void block_To_Index()
--- vfs2vda.c
+++ vfs2vda.c
@@ -6,6 +6,9 @@
 #include <fcntl.h>
 #include "math.h"
 
+#define BUF_SIZE 1024
+#define FILE_SIZE_MULTIPLE 1024
+
 typedef struct FMD {
 	char file_name[248];
 	int file_size;
@@ -19,6 +22,9 @@ typedef union header {
 	}s;
 } Header;
 
+// Header base;
+// Header *freep = NULL;
+
 void vdAllocate(char *diskname, char *filename) {
 	Header *head = malloc(sizeof(Header));
 	
@@ -27,34 +33,52 @@ void vdAllocate(char *diskname, char *filename) {
 	fd = open(filename, O_RDONLY);
 	
 	int size = lseek(fd, 0, SEEK_END);
-	if(size < (2 * 1024 * 1024)) {
+	if(size < (2 * FILE_SIZE_MULTIPLE * FILE_SIZE_MULTIPLE)) {
 		head->fmd.file_size = size;
 	}
 	strcpy(head->fmd.file_name,filename);
-	head->fmd.block_no = 63;
-	
-	unsigned char *buf = (unsigned char *)malloc(sizeof(unsigned char) * head->fmd.file_size);
-	unsigned char *write_buf = (unsigned char *)malloc(sizeof(unsigned char) * 1024);
-	
-	int n;
+	head->fmd.block_no = 64;
+
+	unsigned char *buf = (unsigned char *)malloc(sizeof(unsigned char) * BUF_SIZE);
+	unsigned char *read_buf = (unsigned char *)malloc(sizeof(unsigned char) * BUF_SIZE);
+	unsigned char *write_buf = (unsigned char *)malloc(sizeof(unsigned char) * BUF_SIZE);
+
+	int n, i, count;
+
+	lseek(d, 2048, SEEK_SET);
+	write(d, head, sizeof(Header));
+
 	lseek(fd, 0, SEEK_SET);
-	read(fd, buf, head->fmd.file_size);
-	lseek(d, 62916, SEEK_SET);
-	int i = 0;
-	int count = 0;
+	i = 0;
+	count = 0;
 	while(count < head->fmd.file_size) {
-		while(i < 1024) {
-			write_buf[i] = buf[i];
+		n = read(fd, buf, BUF_SIZE);
+		while(i < BUF_SIZE) {
+			if(i == (BUF_SIZE - 1)) {
+				read_buf = realloc(read_buf, BUF_SIZE);
+			}
+			read_buf[i] = buf[i];
 			i++;
 		}
-		n = write(d, write_buf, 1024);
+		count = count + n;
+	}
+	
+	lseek(d, 62916, SEEK_SET);
+	i = 0;
+	count = 0;
+	while(count < head->fmd.file_size) {
+		while(i < BUF_SIZE) {
+			write_buf[i] = read_buf[i];
+			i++;
+		}
+		n = write(d, write_buf, BUF_SIZE);
 		count = count + n;
 	}
 	
 	lseek(d, 31, SEEK_SET);
 	read(d, buf, 224);
 	
-	int no_blocks = my_ceil((double)head->fmd.file_size/1024);
+	int no_blocks = my_ceil((double)head->fmd.file_size/BUF_SIZE);
 	printf("%d\n", no_blocks);
 	
 	i = 0;
@@ -70,6 +94,7 @@ void vdAllocate(char *diskname, char *filename) {
 	}
 	lseek(d, 31, SEEK_SET);
 	write(d, buf, 224);
+	
 	close(d);
 }
 
