f4058349ded77188b30fde24f5e0d424d544f203
sushrut300596 <43106774+sushrut300596@users.noreply.github.com>
2022-11-04 08:11:40 +0000

    bitmap.c

--- File System/bitmap.c
+++ File System/bitmap.c
@@ -27,7 +27,7 @@ int BitMap_isBitSet(unsigned char *bmap, int block_no) {
     int entry_no = block_no / 8;
     int bit_no = block_no % 8;
     unsigned char e = bmap[entry_no];
-    int ret = (e) & (1 << (8 - 1 - bit_no));
+    int ret = (e) & (1 << (bit_no));
     if(ret != 0) {
         return 1;
     }
@@ -47,25 +47,39 @@ int BitMap_getFreeBlocks(unsigned char *bmap, int size_bitmap) {
     return count;
 }
 
+int BitMap_indexToBlock(int entry, unsigned char bit_num) {
+	int num = (entry * 8) + bit_num;
+	return num;
+}
 
-// int main(int argc, char **argv) {
-//     d = open(argv[1], O_RDONLY);
+
+int main(int argc, char **argv) {
+    d = open(argv[1], O_RDONLY);
     
-//     int sb_size = sizeof(struct SB);
+    int sb_size = sizeof(struct SB);
 
-//     SB *sb = (SB *)malloc(sb_size);
+    SB *sb = (SB *)malloc(sb_size);
 
-//     read(d, sb, sb_size);
+    read(d, sb, sb_size);
 
-//     int no_blocks = ceil((double)sb->size_disk/sb->size_block);
-// 	int md_bytes = (ceil((double)no_blocks/8)) + sb_size;
-// 	int flag_bytes = md_bytes - sb_size;
-// 	int size_bitmap = flag_bytes;
+    int no_blocks = ceil((double)sb->size_disk/sb->size_block);
+	int md_bytes = (ceil((double)no_blocks/8)) + sb_size;
+	int flag_bytes = md_bytes - sb_size;
+	int size_bitmap = flag_bytes;
 
-//     unsigned char *bmap = (unsigned char *)malloc(sizeof(unsigned char) * size_bitmap);
+    unsigned char *bmap = (unsigned char *)malloc(sizeof(unsigned char) * size_bitmap);
 
-//     bmap = Bitmap_to_Array(argv[1]);
+    bmap = Bitmap_to_Array(argv[1]);
     
-//     int free_blocks = BitMap_getFreeBlocks(bmap, size_bitmap);
-//     printf("free_blocks : %d\n", free_blocks);
-// }
+    int free_blocks = BitMap_getFreeBlocks(bmap, size_bitmap);
+
+    int i;
+    int *block_nos = (int *)malloc(sizeof(int) * size_bitmap);
+    for(i = 0; i < size_bitmap; i++) {
+        block_nos[i] = BitMap_indexToBlock(i, bmap[i]);
+        printf("%d\t", block_nos[i]);
+    }
+    printf("\n");
+
+    printf("free_blocks : %d\n", free_blocks);
+}
